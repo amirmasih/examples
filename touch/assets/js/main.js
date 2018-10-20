8605621d043f017d25a7c76c05e2e486bd4eaa75
Corey Huddleston <coreyhuddleston@Coreys-MacBook-Pro.local>
2018-10-20 20:47:48 +0530

    Test

---
+++ touch/assets/js/main.js
@@ -0,0 +1,9 @@
+// Put your javascript or jquery here.
+// Remember to comment well. Sections, which page it's used on, and what each function does, and in most cases what each line does.
+
+$(document).ready(function() {
+  $('.js-hover').on('touchstart touchend', function(e) {
+      e.preventDefault();
+      $(this).toggleClass('hover_effect');
+  });
+});
