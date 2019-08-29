d10a56d78a5c619309dd8e2bab60549b2bd0b163
amirmasih <godplzforgiveme@gmail.com>
2019-08-29 16:17:29 +0530

    Create rss-test.php

---
+++ rss-test.php
@@ -0,0 +1,32 @@
+<?php
+
+<rss version="2.0">
+
+<channel>
+
+  <item>
+    <title></title>
+    <link></link>
+    <category>Sermon</category>
+    <pub:date></pubDate>
+    <dc:creator>HeartCry</dc:creator>
+    <!-- Image -->
+    <enclosure url="#" />
+  <item>
+
+  <!-- Small Cards -->
+  <item>
+    <title></title>
+    <link></link>
+    <category>Mission Update</category>
+    <pub:date></pubDate>
+    <dc:creator>HeartCry</dc:creator>
+    <!-- Image -->
+    <enclosure url="#" />
+  </item>
+
+</channel>
+
+</rss>
+
+?>
